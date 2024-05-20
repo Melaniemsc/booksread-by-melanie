688300c830a884ee5dcb44fbe6d1e9b72caf23bc
Melaniemsc <melsan1706@gmail.com>
2024-05-20 08:47:32 +0100

    books read styles added

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const BooksRead = require('./models/booksRead')
 const port = 3000
 const methodOverride = require("method-override");
 const morgan = require("morgan")
-const path = require("path")
+const path = require("path");
+const booksRead = require('./models/booksRead');
+const { log } = require('console');
 const app = express();
 mongoose.connect(process.env.MONGODB_URI)
 
@@ -39,6 +41,20 @@ app.get('/booksRead/:booksReadId/edit', async (req, res) => {
   })
 })
 
+app.get('/search', async (req,res) =>{
+
+  res.render('./search.ejs',)
+}) 
+
+app.get('/searchResults', async (req,res) =>{
+  const booksResult = await BooksRead.find({name:})
+  console.log(req);
+  res.render('./searchResults.ejs', {
+    booksResult,
+  })
+})
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -48,20 +64,21 @@ app.use(express.static(path.join(__dirname,"public")))
 
 
 app.post('/booksRead', async (req, res) => {
-  const book = await BooksRead.create(req.body)
+  const isDuplicate = await BooksRead.findOne({name: req.body.name})
+  if (!isDuplicate){
+    await BooksRead.create(req.body)
+  }
   res.redirect('/booksRead')
 })
 
-
 app.delete('/booksRead/:booksReadId', async (req, res) => {
-  const book = await BooksRead.findByIdAndDelete(req.params.booksReadId)
-  console.log(book);
+  await BooksRead.findByIdAndDelete(req.params.booksReadId)
   res.redirect('/booksRead')
 })
 
 app.put('/booksRead/:booksReadId', async (req, res) => {
-  const book = await BooksRead.findByIdAndUpdate(req.params.booksReadId, req.body)
-  res.redirect(`/booksRead`)
+  await BooksRead.findByIdAndUpdate(req.params.booksReadId, req.body)
+  res.redirect('/booksRead')
 })
 
 app.listen(port, () => {
